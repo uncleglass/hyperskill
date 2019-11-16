969114f4ef4de45d2e930ed7168070b0b5001833
uncleglass <bartekkiliszek@gmail.com>
2019-11-16 17:54:22 +0100

    Change method implementation
    It is a need to desplay '.' instead of default in PL ','

--- readability-score/src/main/java/readability/utils/userinterface/MessagePreparatorUtil.java
+++ readability-score/src/main/java/readability/utils/userinterface/MessagePreparatorUtil.java
@@ -3,7 +3,13 @@ package readability.utils.userinterface;
 import readability.statistics.TextStatistics;
 import readability.utils.index.ReadabilityIndexUtil;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class MessagePreparatorUtil {
+    private static DecimalFormat format = new DecimalFormat("#.##", DecimalFormatSymbols.getInstance(Locale.US));
+
     private MessagePreparatorUtil() {
     }
 
@@ -11,7 +17,7 @@ public class MessagePreparatorUtil {
         return "Words: " + textStatistics.getWords() +
                 "\nSentences: " + textStatistics.getSentences() +
                 "\nCharacters: " + textStatistics.getCharacters() +
-                "\nThe score is: " + String.format("%.2f", textStatistics.getReadabilityIndex()) +
+                "\nThe score is: " + format.format( textStatistics.getReadabilityIndex()) +
                 "\nThis text should be understood by " +
                 ReadabilityIndexUtil.getApproximateAge(textStatistics.getReadabilityIndex()) +
                 " year olds.";
