c759bf497057b49431a54c55628f3de2616c0389
uncleglass <bartekkiliszek@gmail.com>
2019-11-16 17:35:33 +0100

    Change implemantation of readFile method
    There was problem with printing text. Reading by this method caused
    printing bytes values, not a string.

--- readability-score/src/main/java/readability/Main.java
+++ readability-score/src/main/java/readability/Main.java
@@ -3,8 +3,6 @@ package readability;
 import readability.statistics.TextStatistics;
 import readability.utils.file.ReaderUtil;
 
-import java.io.IOException;
-
 import static readability.utils.parser.ParserUtil.parseToStatistics;
 import static readability.utils.userinterface.MessagePreparatorUtil.prepareMessageForText;
 import static readability.utils.userinterface.MessagePreparatorUtil.prepareMessageForTextStatistics;
@@ -13,15 +11,10 @@ import static readability.utils.userinterface.UserInterfaceUtil.print;
 public class Main {
     public static void main(String[] args) {
         if (args.length > 0) {
-            try {
                 String text = ReaderUtil.readFile(args[0]);
                 print(prepareMessageForText(text));
                 TextStatistics textStatistics = parseToStatistics(text);
                 print(prepareMessageForTextStatistics(textStatistics));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
--- readability-score/src/main/java/readability/utils/file/ReaderUtil.java
+++ readability-score/src/main/java/readability/utils/file/ReaderUtil.java
@@ -3,14 +3,21 @@ package readability.utils.file;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 
 public class ReaderUtil {
     private ReaderUtil() {
     }
 
-    public static String readFile(final String fileName) throws IOException {
-        return Arrays.toString(Files.readAllBytes(Paths.get(fileName)));
+    public static String readFile(final String fileName) {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(s -> contentBuilder.append(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
     }
 }
