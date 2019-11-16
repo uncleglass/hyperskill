ebd41ae1a7bd1e653f178d87e13eec9f4074de5a
uncleglass <bartekkiliszek@gmail.com>
2019-11-16 11:56:07 +0100

    Added class for text statistics

---
+++ readability-score/src/main/java/readability/statistics/TextStatistics.java
@@ -0,0 +1,29 @@
+package readability.statistics;
+
+public class TextStatistics {
+    private final int characters;
+    private final int words;
+    private final int sentences;
+
+    public TextStatistics(int characters, int words, int sentences) {
+        this.characters = characters;
+        this.words = words;
+        this.sentences = sentences;
+    }
+
+    public double getReadabilityIndex() {
+        return 0d;
+    }
+
+    public int getCharacters() {
+        return characters;
+    }
+
+    public int getWords() {
+        return words;
+    }
+
+    public int getSentences() {
+        return sentences;
+    }
+}
