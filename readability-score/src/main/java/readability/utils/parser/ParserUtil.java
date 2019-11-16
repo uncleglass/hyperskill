62c86ab15321c9d9ea89b073f012ae32f536c2c1
uncleglass <bartekkiliszek@gmail.com>
2019-11-16 14:02:23 +0100

    Added  implementation to parseToStatistics method

--- readability-score/src/main/java/readability/utils/parser/ParserUtil.java
+++ readability-score/src/main/java/readability/utils/parser/ParserUtil.java
@@ -7,6 +7,18 @@ public class ParserUtil {
     }
 
     public static TextStatistics parseToStatistics(final String text) {
-        return new TextStatistics(0,0,0);
+        String[] sentencesArray = text.split("[.!?]");
+        int wordsCounter = 0;
+        for (String sentence : sentencesArray) {
+            String[] words = sentence.trim().split(" ");
+            wordsCounter += words.length;
+        }
+        int characterCounter = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (!Character.isWhitespace(text.charAt(i))) {
+                characterCounter++;
+            }
+        }
+        return new TextStatistics(characterCounter,wordsCounter,sentencesArray.length);
     }
 }
